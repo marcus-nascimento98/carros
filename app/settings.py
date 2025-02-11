a267aef36c9ba68ab92d087f1c623457683c0424
marcus-nascimento98 <marcus_nascimento@live.com>
2025-02-11 11:08:39 -0300

    config allowed_hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wr9ed)z^*$4)!8@%*9&qmjwq73bhzrk^!o@&049(@dtrdkqkrz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
